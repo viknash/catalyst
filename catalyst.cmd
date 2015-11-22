7e23093a03b249be7c6c71d22ca0f3372922c62f
Viknash <viknash@hobbiz.com>
2015-11-22 22:06:28 +0800

    Added dependencies

--- catalyst.cmd
+++ catalyst.cmd
@@ -31,6 +31,7 @@ set INCLUDE=%CD%\extern\math\include;%INCLUDE%
 set INCLUDE=%CD%\extern\predef\include;%INCLUDE%
 set INCLUDE=%CD%\extern\smart_ptr\include;%INCLUDE%
 set INCLUDE=%CD%\extern\system\include;%INCLUDE%
+set INCLUDE=%CD%\extern\chrono\include;%INCLUDE%
 doskey devenv=devenv.exe /useenv $*
 %comspec% /k ""%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat"" x86
 popd
