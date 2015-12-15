c0a2846c2cb7615d92b3518bac179c1945357c49
viknash <viknash@hobbiz.com>
2015-12-15 17:56:47 +0800

    Update menu

--- scripts/devmenu.js
+++ scripts/devmenu.js
@@ -23,26 +23,50 @@ menu.addDelimiter('-', 80, 'Catalyst Help')
         function () {
             var grep = spawn('devenv');
             grep.on('close', function (code, signal) {
-                console.log('Visual Studio terminated with' + signal);
+                console.log('Exit: ' + signal);
             });
         })
     .addItem(
         'Launch Explorer',
         function () {
-            var grep = spawn('explorer',[process.cwd()]);
+            var grep = spawn('explorer',[process.cwd()+"\\.."]);
             grep.on('close', function (code, signal) {
-                console.log('Explorer terminated with' + signal);
+                console.log('Exit: ' + signal);
             });
         })
     .addDelimiter('*', 80)
+    .addItem(
+        'Generate Catalyst Solution',
+        function () {
+            var grep = spawn('fbuild', ['solution'],{cwd:process.cwd()+'\\..\\src'});
+			grep.stdout.on('data', function (data) {
+			  console.log(''+data);
+			});			
+        })
     .addItem(
         'Launch Catalyst Solution',
         function () {
             var grep = spawn('devenv', ['..\\tmp\\VisualStudio\\Catalyst.sln']);
             grep.on('close', function (code, signal) {
-                console.log('Visual Studio terminated with' + signal);
-            });
+                console.log('Exit: ' + signal);
+            });			
         })
+    .addItem(
+        'Launch libCDS',
+        function () {
+            var grep = spawn('devenv', ['..\\extern\\libcds\\projects\\Win\\vc14\\cds.sln']);
+            grep.on('close', function (code, signal) {
+                console.log('Exit: ' + signal);
+            });			
+        })
+    .addItem(
+        'Deploy Boost',
+        function () {
+            var grep = spawn('deploy.cmd', [],{cwd:process.cwd()+'\\..\\extern\\boost'});
+			grep.stdout.on('data', function (data) {
+			  console.log(''+data);
+			});			
+        })			
     .addDelimiter('*', 80)
     .disableDefaultHeader() 
     .start();
